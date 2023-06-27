822a2d2e176a671b68b7e304101b0fbf56cfe7e6
Mael-221 <mohamed-amine.el-maghraoui@epita.fr>
2023-06-27 20:30:51 +0200

    generation de données aléatoire pour les drones

--- src/main/scala/producers.scala
+++ src/main/scala/producers.scala
@@ -1,12 +1,15 @@
 import java.util.Properties
 import org.apache.kafka.clients.producer._
+import scala.util.Random
 
-object ProducerApp {
+object DroneSimulator {
   def main(args: Array[String]): Unit = {
     val props = createProperties()
     val producer = new KafkaProducer[String, String](props)
-    val droneData = "droneId,location,citizenNames,harmonyScore,wordsHeard"
-    sendRecord(producer, droneData)
+
+    // Simulate data for 100 drones
+    simulateDrones(producer, 100)
+
     producer.close()
   }
 
@@ -18,8 +21,25 @@ object ProducerApp {
     props
   }
 
+  def generateDroneData(): String = {
+    val droneId = Random.nextInt(1000) // Generate a random drone ID
+    val location = s"${Random.nextInt(100)},${Random.nextInt(100)}" // Generate a random location
+    val citizenNames = List.fill(5)(s"Citizen${Random.nextInt(1000)}").mkString(",") // Generate random citizen names
+    val harmonyScore = Random.nextDouble() * 100 // Generate a random harmony score
+    val wordsHeard = List.fill(5)(s"Word${Random.nextInt(1000)}").mkString(",") // Generate random words heard
+    s"$droneId,$location,$citizenNames,$harmonyScore,$wordsHeard"
+  }
+
   def sendRecord(producer: KafkaProducer[String, String], droneData: String): Unit = {
     val record = new ProducerRecord[String, String]("droneData", "key", droneData)
     producer.send(record)
+  }
+
+  def simulateDrones(producer: KafkaProducer[String, String], numDrones: Int): Unit = {
+    if (numDrones > 0) {
+      val droneData = generateDroneData()
+      sendRecord(producer, droneData)
+      simulateDrones(producer, numDrones - 1)
+    }
   }
 }
