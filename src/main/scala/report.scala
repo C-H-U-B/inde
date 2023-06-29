a653853bf36da903aba671825341e9c65698ea1c
Mael-221 <mohamed-amine.el-maghraoui@epita.fr>
2023-06-29 02:13:10 +0200

    partie 3 qui ne fonctionne pas (pb de version avec spark)

--- src/main/scala/report.scala
+++ src/main/scala/report.scala
@@ -1,56 +1,73 @@
-import java.util.Properties
-import org.apache.kafka.clients.consumer.{ConsumerRecord, KafkaConsumer}
-import scala.collection.JavaConverters._
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.SparkConf
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.kafka.common.serialization.StringDeserializer
 import play.api.libs.json._
+import java.nio.file.{Files, Paths}
+import java.nio.charset.StandardCharsets
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.PutObjectRequest
+import software.amazon.awssdk.regions.Region
 
-object DroneReportConsumer {
-  implicit val citizenReads: Reads[Citizen] = Json.reads[Citizen]
-  implicit val droneReads: Reads[Drone] = Json.reads[Drone]
+object DroneReportConsumerSpark {
+  val spark: SparkSession = SparkSession.builder.master("local[*]").appName("DroneReportConsumerSpark").getOrCreate()
+  spark.sparkContext.hadoopConfiguration.set("fs.s3a.access.key", sys.env("AWS_ACCESS_KEY"))
+  spark.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key", sys.env("AWS_SECRET_KEY"))
+
+  import spark.implicits._
 
   def main(args: Array[String]): Unit = {
-    val props = createConsumerProperties()
-    val consumer = new KafkaConsumer[String, String](props)
-    consumer.subscribe(java.util.Collections.singletonList("droneData"))
+    val ssc = new StreamingContext(spark.sparkContext, Seconds(60))
 
-    consumeRecords(consumer)
-  }
+    val kafkaParams: Map[String, Object] = Map(
+      "bootstrap.servers" -> "localhost:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "reportGroup",
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
 
-  def createConsumerProperties(): Properties = {
-    val props = new Properties()
-    props.put("bootstrap.servers", "localhost:9092")
-    props.put("group.id", "reportGroup")
-    props.put("enable.auto.commit", "false")
-    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-    props
-  }
+    val topics = Array("droneData")
+    val stream = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      LocationStrategies.PreferConsistent,
+      ConsumerStrategies.Subscribe[String, String](topics, kafkaParams)
+    )
 
-  def consumeRecords(consumer: KafkaConsumer[String, String]): Unit = {
-    val records = consumer.poll(java.time.Duration.ofMillis(100)).asScala.toList
-    if (records.nonEmpty) {
-      processRecords(records)
-      consumer.commitSync()
-    }
-    consumeRecords(consumer)
-  }
+    stream.foreachRDD { rdd =>
+      rdd.map(record => Json.parse(record.value())).foreachPartition { iter =>
+        val droneData = iter.flatMap { json =>
+          val timestamp = (json \ "timestamp").as[String]
+          val drone = (json \ "drone").as[JsObject]
+          val droneId = (drone \ "id").as[Int]
+          val droneLocation = (drone \ "location").as[Seq[Int]]
+          Some((timestamp, droneId, droneLocation(0), droneLocation(1)))
+        }.toSeq.toDF("timestamp", "drone_id", "drone_x", "drone_y")
 
-  def processRecords(records: List[ConsumerRecord[String, String]]): Unit = records match {
-    case record :: remainingRecords =>
-      val data = Json.parse(record.value())
-      val timestamp = (data \ "timestamp").as[String]
-      val drone = (data \ "drone").as[Drone]
-      val citizens = (data \ "citizens").asOpt[Seq[Citizen]].getOrElse(Seq.empty[Citizen])
+        val citizenData = iter.flatMap { json =>
+          val timestamp = (json \ "timestamp").as[String]
+          val drone = (json \ "drone").as[JsObject]
+          val droneId = (drone \ "id").as[Int]
+          val droneLocation = (drone \ "location").as[Seq[Int]]
+          val citizens = (json \ "citizens").asOpt[Seq[JsObject]].getOrElse(Seq.empty[JsObject])
+          citizens.map { citizen =>
+            val name = (citizen \ "name").as[String]
+            val harmonyScore = (citizen \ "harmonyScore").as[Double]
+            val words = (citizen \ "words").as[Seq[String]]
+            (timestamp, droneId, droneLocation(0), droneLocation(1), name, harmonyScore, words.mkString(","))
+          }
+        }.toSeq.toDF("timestamp", "drone_id", "drone_x", "drone_y", "citizen_name", "harmony_score", "words")
 
-      if (citizens.nonEmpty) {
-        citizens.foreach { citizen =>
-          println(s"Timestamp: $timestamp, Drone ID: ${drone.id}, Location: ${drone.location}, Citizen: ${citizen.name}, Harmony Score: ${citizen.harmonyScore}, Words: ${citizen.words.mkString(", ")}")
-        }
-      } else {
-        println(s"Timestamp: $timestamp, Drone ID: ${drone.id}, Location: ${drone.location}, No citizen detected.")
+        droneData.write.mode(SaveMode.Append).csv("s3a://inde-bucket/droneData")
+        citizenData.write.mode(SaveMode.Append).csv("s3a://inde-bucket/citizenData")
       }
+    }
 
-      processRecords(remainingRecords)
-
-    case Nil => // No more records to process
+    ssc.start()
+    ssc.awaitTermination()
   }
 }
